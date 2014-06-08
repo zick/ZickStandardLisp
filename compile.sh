345e77b43150f4e106a5721ebaece5239e8a414c
zick <zickminoh@gmail.com>
2014-06-08 20:53:44 +0900

    Support both command-line flags and stdin. Add fib.lsp as a sample program.

--- compile.sh
+++ compile.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-cat lisp.lsp | sed -e 's/;.*$//g' | tr -d '\n' | sed -e 's/  */ /g' | \
-    awk "{sub(\"WRITE_HERE\", \"$1\")}{print}"
+while getopts e:h OPT; do
+  case $OPT in
+    "e") exp=$OPTARG;;
+    "h") help=1;;
+  esac
+done
+if [ -z "$exp" -a -z "$help" ]; then
+  exp=`cat - | tr -d '\n'`
+fi
+if [ -z "$exp" ]; then
+  cat <<EOF
+Usage:
+  ./compile.sh "(car '(a b c))" > car.zsl
+    or
+  ./compile.sh < fib5.lsp > fib5.zsl
+EOF
+else
+  cat lisp.lsp | sed -e 's/;.*$//g' | tr -d '\n' | sed -e 's/  */ /g' | \
+      awk "{sub(\"WRITE_HERE\", \"$exp\")}{print}"
+fi
---
+++ fib5.lsp
@@ -0,0 +1,1 @@
+((lambda(x) (defun fib (n) (if (eq n 1) 1 (if (eq n 0) 1 (+ (fib(- n 1)) (fib(- n 2)))))) (fib x)) 8)
